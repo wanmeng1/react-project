4375636f89237e23ce3a595fded0acb0ebff0417
wanmeng <986953431@qq.com>
2018-09-30 10:25:43 +0800

    'demo'

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Demo from './pages/Day1/Demo'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Demo />, document.getElementById('root'));
 registerServiceWorker();
